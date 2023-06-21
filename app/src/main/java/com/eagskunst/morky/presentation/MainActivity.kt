748d0977ce29118ce19ebf746962e47970a1caa3
Emmanuel Guerra <emmanuelnypd@gmail.com>
2023-06-21 19:32:31 -0400

    [FEAT] Fix error and progress components

--- app/src/main/java/com/eagskunst/morky/presentation/MainActivity.kt
+++ app/src/main/java/com/eagskunst/morky/presentation/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,11 +22,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.palette.graphics.Palette
+import com.eagskunst.morky.R
 import com.eagskunst.morky.ui.component.CharactersPagerWithInfoCard
 import com.eagskunst.morky.ui.theme.MorkyTheme
 import com.skydoves.landscapist.components.LocalImageComponent
@@ -71,15 +82,42 @@ fun MainView(viewModel: MainViewModel = hiltViewModel(), backgroundPalette: Pale
             palette = backgroundPalette,
         )
 
-        is MainViewState.Error -> Text("Error: ${state.cause}")
-        MainViewState.Loading -> CircularProgressIndicator()
+        is MainViewState.Error -> ErrorComponent(
+            modifier = Modifier.padding(20.dp),
+        ) {
+            viewModel.getCharacters()
+        }
+
+        MainViewState.Loading -> Box {
+            CircularProgressIndicator(
+                modifier = Modifier.size(50.dp).align(Alignment.Center),
+            )
+        }
+    }
+}
+
+@Composable
+fun ErrorComponent(
+    modifier: Modifier = Modifier,
+    message: String = LocalContext.current.getString(R.string.error_message),
+    onRetryClick: () -> Unit = {},
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = message, textAlign = TextAlign.Center)
+        ElevatedButton(onClick = onRetryClick) {
+            Text(LocalContext.current.getString(R.string.retry_label))
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ErrorComponentPreview() {
     MorkyTheme {
-        MainView(backgroundPalette = null)
+        ErrorComponent()
     }
 }
